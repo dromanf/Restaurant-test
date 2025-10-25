f64bb34ee7c32aaa7225d03e48c69d9cd22489f7
Deybys Roman <romandeybys@gmail.com>
2025-10-25 15:14:16 -0400

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Restaurant-test/', // Necesario para GitHub Pages
   plugins: [vue()],
   resolve: {
     alias: {
